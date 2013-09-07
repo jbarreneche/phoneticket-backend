8802ed596d2bf7ab87b5d479b8cecc64fd676e87
Juan Manuel Barreneche <snipperme@gmail.com>
2013-09-07 16:30:07 -0300

    Add test for unique email

--- test/integration/api_signup_test.rb
+++ test/integration/api_signup_test.rb
@@ -13,6 +13,19 @@ class ApiSignupTest < ActionDispatch::IntegrationTest
     assert_equal json_response["email"], "someone@signinup.com"
   end
 
+  test "doesnt allow duplicate emails" do
+    user = users("first_user")
+
+    post "/api/users", {
+      email: user.email,
+      password: "123456"
+    }, "HTTP_ACCEPT" => "application/json"
+
+    assert_equal 422, status
+
+    assert json_response["errors"].has_key?("email")
+  end
+
   def json_response
     JSON(response.body)
   end
