914b123fd8014d2af8ac507530d760ce14d59a66
Juan Manuel Barreneche <snipperme@gmail.com>
2013-09-07 23:29:26 -0300

    Add some tests and fix simplecov coverage

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :users
+
+  test "#disable! disables user for authentication" do
+    user = users("first_user")
+    assert user.disable!
+    refute user.valid_for_authentication?
+  end
+
+  test "#enable! reenables user for authentication" do
+    user = users("first_user")
+    assert user.disable! && user.enable!
+    assert user.valid_for_authentication?
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,3 @@
-if ENV['COVERAGE']
-  require 'simplecov'
-  SimpleCov.start "rails"
-end
-
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
