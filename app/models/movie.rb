9ee8a49862bee1198aebbb8306544d49cdf28168
Juan Manuel Barreneche <snipperme@gmail.com>
2013-09-20 23:57:48 -0300

    Remove column ambiguity

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ActiveRecord::Base
   has_many :shows, dependent: :restrict_with_error
 
   scope :join_active_shows, -> { joins(:shows).merge(Show.active) }
-  scope :with_active_shows, -> { join_active_shows.group(:id).having("count(shows.id) > 0") }
+  scope :with_active_shows, -> { join_active_shows.group("movies.id").having("count(shows.id) > 0") }
 
   def active_shows
     shows.active
