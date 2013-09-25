076bc27690e089765b54d732585a85a5ddf44c7f
Juan Manuel Barreneche <snipperme@gmail.com>
2013-09-24 21:09:44 -0300

    Missing validations and translations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,12 @@ class Movie < ActiveRecord::Base
   has_many :shows, dependent: :restrict_with_error
   mount_uploader :cover, CoverUploader
 
+  validates_presence_of :title
+
+  validates_format_of :youtube_trailer,
+    with:  %r{\Ahttp://www\.youtube\.com/watch\?v=([a-zA-Z0-9_-]*)\z},
+    allow_blank: true
+
   scope :join_active_shows, -> { joins(:shows).merge(Show.active) }
   scope :with_active_shows, -> { join_active_shows.group("movies.id").having("count(shows.id) > 0") }
 
