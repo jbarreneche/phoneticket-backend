36cab15decd053bbeae7b706bd49c65b16e6d895
Juan Manuel Barreneche <snipperme@gmail.com>
2013-09-28 22:20:39 -0300

    Allow filtering movie shows by theatre_id

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -7,6 +7,9 @@ class Api::MoviesController < Api::BaseController
 
   def show
     @movie = Movie.find(params[:id])
+    @shows = @movie.active_shows
+    @shows = @shows.joins(:room).where(rooms: {theatre_id: params[:theatre_id]}) if params[:theatre_id]
+
     respond_with @movie
   end
 
