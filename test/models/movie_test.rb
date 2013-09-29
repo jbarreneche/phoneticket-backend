586fb9dc0d594abfb5c4f2fe8b8eff93de352b56
Juan Barreneche <snipperme@gmail.com>
2013-09-29 12:15:30 -0300

    Increase test coverage

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "setting the cast as a text separated with \n" do
+    movie = Movie.new
+    refute movie.valid?
+    refute movie.errors[:cast_raw].empty?
+
+    movie.cast_raw = <<-CAST
+      Actor one
+    CAST
+    movie.valid?
+
+    assert movie.errors[:cast_raw].empty?
+    assert_equal ["Actor One"], movie.cast
+    assert_equal "Actor One", movie.cast_raw
+  end
 end
--- test/models/show_test.rb
+++ test/models/show_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ShowTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "it's name is the movie title with the room label" do
+    room = Room.new(name: "Room")
+    movie = Movie.new(title: "Some movie")
+    show = Show.new(movie: movie, room: room)
+    assert show.name.include?(movie.title)
+    assert show.name.include?(room.to_label)
+  end
 end
