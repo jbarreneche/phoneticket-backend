86a0f31c5cbcc4e0fa466d1f1264496684108b58
Juan Barreneche <snipperme@gmail.com>
2013-11-09 09:31:16 -0300

    Add create action for rooms

--- app/helpers/room_helper.rb
+++ app/helpers/room_helper.rb
@@ -4,4 +4,10 @@ module RoomHelper
     I18n.t("room.shapes.#{shape}", default: shape.titleize) if shape
   end
 
+  def shapes_collection
+    Shape::CONFIGS.keys.map do |shape|
+      [human_shape(shape), shape]
+    end
+  end
+
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,8 @@ class Room < ActiveRecord::Base
   belongs_to :theatre
   has_many :shows
   composed_of :room_shape, class_name: "Shape", constructor: :for_name, mapping: %w[shape name]
+  validates_uniqueness_of :name, scope: :theatre_id
+  validates_presence_of :theatre, :name, :shape
 
   def to_label
     "#{name} (#{theatre})"
